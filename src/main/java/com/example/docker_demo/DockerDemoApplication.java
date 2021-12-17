52a711a963917a0bf7111a8c0fa6a39c47d934fb
Ria Madhyan <riamadhyan@hotmail.com>
2021-12-17 15:50:07 +0530

    added code

--- src/main/java/com/example/docker_demo/DockerDemoApplication.java
+++ src/main/java/com/example/docker_demo/DockerDemoApplication.java
@@ -2,12 +2,21 @@ package com.example.docker_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@RestController
 public class DockerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+
+	@GetMapping("/welcome")
+	public String hello() {
+		return "Welcome World";
 	}
 
 }
